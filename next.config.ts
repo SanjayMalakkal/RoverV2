99d7f6c77054cfe63ee1661c091befa5d7861af8
rahul v <rahul@vizru.com>
2025-11-14 14:51:10 +0530

    basepath updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+   basePath: "/rover-ebook/",
+  assetPrefix: "/rover-ebook/",
   /* config options here */
 };
 
