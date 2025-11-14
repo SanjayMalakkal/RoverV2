ac6e336623394c9ed8600540f853acc0987855cc
rahul v <rahul@vizru.com>
2025-11-14 14:42:53 +0530

    Config changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
 };
 
@@ -9,7 +10,7 @@ export default nextConfig;
 
 // next.config.js
 
-module.exports = {
+/* module.exports = {
   async rewrites() {
     return [
       {
@@ -18,5 +19,5 @@ module.exports = {
       },
     ];
   },
-};
+}; */
 
